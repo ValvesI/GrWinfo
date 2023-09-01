47bc15b02991f3693d3554107f4579602f614919
ValvesI <131627287+ValvesI@users.noreply.github.com>
2023-09-01 19:29:35 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+//Alerta
+alert('Testosterona ixtouradassa, esqueça tudo');
+
+//pom
 function tocasompom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocasompom;
-alert('Testosterona ixtouradassa, esqueça tudo');
+
+
+const listadeteclas = document.querySelectorAll('.tecla');
+
 
