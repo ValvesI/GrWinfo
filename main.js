ad823b70b7473b6dbe72a8710084796af98941b7
ValvesI <131627287+ValvesI@users.noreply.github.com>
2023-09-01 20:43:46 -0300

    .

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let contador = 0
 while (contador < listadeteclas.length) {
 
  const tecla = listadeteclas[contador];
- const analise = tecla[contador].classList[1];
+ const analise = tecla.classList[1];
 
     const idaudio = `#som_${analise}`;
 
