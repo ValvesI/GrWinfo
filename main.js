318c372fd39bcfd407478c850f89a489a8206e7b
ValvesI <131627287+ValvesI@users.noreply.github.com>
2023-09-03 18:39:20 -0300

    .

--- main.js
+++ main.js
@@ -26,4 +26,10 @@ for (let contador = 0; contador < listadeteclas.length; contador++) {
         tocasom(idaudio);
     }
     //console.log(contador);
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
