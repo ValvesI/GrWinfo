1ffaf7bfb6f6d65d9feaa5f6528a99652ef33120
ValvesI <131627287+ValvesI@users.noreply.github.com>
2023-09-14 08:03:13 -0300

    .

--- main.js
+++ main.js
@@ -4,6 +4,6 @@
 function toca () {
     document.querySelector('#som_tecla_pom').play();
 }
-const lista=document.quersySelectorAll('.tecla');
+const lista=document.querySelectorAll('.tecla');
 
 document.querySelector('.tecla_pom').onclick = toca;
