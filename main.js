b10e3d22e37cf6c2088ebdce987f81190a6523d9
ValvesI <131627287+ValvesI@users.noreply.github.com>
2023-10-06 10:31:49 -0300

    .

--- main.js
+++ main.js
@@ -23,8 +23,10 @@ while (Contador < lista.length) {
 Contador = Contador + 1;
 //console.log(Contador);
 tecla.onkeydown = function (evento) {
+    if (evento.code === 'Space' || evento.code === 'Enter') {
     tecla.classList.add('ativa');
     console.log(evento.code === 'Space' || evento.code === 'Enter');
+}
 }
 
 tecla.onkeyup = function () {
